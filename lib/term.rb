43be5aba2dc221f483b04e95426a7f705650fafd
Tom Stuart <tom@codon.com>
2017-06-03 13:05:26 +0100

    Replace sequence typing rule with derived form

--- lib/term.rb
+++ lib/term.rb
@@ -11,4 +11,43 @@ module Term
   Unit = Object.new
   Variable = Struct.new(:name)
   Zero = Object.new
+
+  def self.free_names(term)
+    case term
+    when Abstraction
+      free_names(term.body) - [term.parameter_name]
+    when Application
+      free_names(term.left) + free_names(term.right)
+    when If
+      free_names(term.condition) + free_names(term.consequent) + free_names(term.alternative)
+    when Sequence
+      free_names(term.first) + free_names(term.second)
+    when True, False
+      []
+    when Unit
+      []
+    when Variable
+      term.name
+    else
+      raise "can’t find free names for #{term}"
+    end
+  end
+
+  def self.all_names
+    0.step.lazy.map { |n| n.times.inject('a') { |s| s.succ } }
+  end
+
+  def self.fresh_name(term)
+    names = free_names(term)
+    all_names.detect { |name| !names.include?(name) }
+  end
+
+  def self.desugar(term)
+    case term
+    when Sequence
+      Application.new(Abstraction.new(fresh_name(term.second), Type::Unit, term.second), term.first)
+    else
+      raise "can’t desugar #{term}"
+    end
+  end
 end
