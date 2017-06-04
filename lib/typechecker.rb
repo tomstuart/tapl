4e1a66bf6c27551f6f6d85e30707f11c7a6f1fb8
Tom Stuart <tom@codon.com>
2017-06-04 19:19:53 +0100

    Typecheck natural numbers

--- lib/typechecker.rb
+++ lib/typechecker.rb
@@ -8,6 +8,16 @@ module Typechecker
     case term
     when Term::False, Term::True
       Type::Boolean
+    when Term::IsZero
+      term_type = type_of(term.term)
+      raise Error, "#{term.term} isn’t a natural number" unless term_type == Type::NaturalNumber
+      Type::Boolean
+    when Term::Pred, Term::Succ
+      term_type = type_of(term.term)
+      raise Error, "#{term.term} isn’t a natural number" unless term_type == Type::NaturalNumber
+      Type::NaturalNumber
+    when Term::Zero
+      Type::NaturalNumber
     else
       fail "can’t typecheck #{term}"
     end
--- spec/typechecking_spec.rb
+++ spec/typechecking_spec.rb
@@ -12,4 +12,30 @@ RSpec.describe 'typechecking' do
       expect('false').to typecheck.as('Bool')
     end
   end
+
+  describe 'natural numbers' do
+    example do
+      expect('0').to typecheck.as('Nat')
+    end
+
+    example do
+      expect('pred 0').to typecheck.as('Nat')
+    end
+
+    example do
+      expect('succ (pred 0)').to typecheck.as('Nat')
+    end
+
+    example do
+      expect('iszero (succ (pred 0))').to typecheck.as('Bool')
+    end
+
+    example do
+      expect('pred (iszero 0)').not_to typecheck
+    end
+
+    example do
+      expect('iszero (iszero 0)').not_to typecheck
+    end
+  end
 end
